d48226ff866912042d3e2195abf654ccda4e9c81
Nguyễn Bảo Trung <baotrung372k@gmail.com>
2023-11-16 09:58:17 +0700

    update| (#3)
    Co-authored-by: trungnb37 <trungnb@vtn-global.com>

--- lib/data/services/network_services/api_constants.dart
+++ lib/data/services/network_services/api_constants.dart
@@ -1,5 +1,5 @@
-abstract class ApiConstants {
-  static const baseUrl = 'https://moviesdatabase.p.rapidapi.com/';
+class ApiConstants {
+  static const baseUrl = 'https://moviesdatabase.p.rapidapi.com';
   static const apiKey = 'a361fcde14msh52288b93c723fc9p1b2e7ejsn560b55fa324c';
   static const apiHost = 'moviesdatabase.p.rapidapi.com';
 }
