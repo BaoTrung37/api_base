d7bd40f80e204caf5ee626577ffb57dd0e5934e3
BaoTrung <baotrung372k@gmail.com>
2023-11-16 22:59:30 +0700

    add

---
+++ lib/domain/entities/utils/genres_entity.dart
@@ -0,0 +1,3 @@
+class GenresEntity{
+  
+}
---
+++ lib/domain/use_cases/utils/get_util_genres_use_case.dart
@@ -0,0 +1,8 @@
+import 'package:api_base/domain/use_cases/use_case.dart';
+
+class GetUtilGenresUseCase extends FutureOutputUseCase {
+  @override
+  Future run() {
+    throw UnimplementedError();
+  }
+}
