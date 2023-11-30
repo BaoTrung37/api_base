fae5f4c41d7401bfaa1a131e2689a3fba9855a09
trungnb37 <trungnb@vtn-global.com>
2023-11-30 15:06:23 +0700

    refactor code

--- lib/presentation/pages/sign_in/screen/sign_in_screen.dart
+++ lib/presentation/pages/sign_in/screen/sign_in_screen.dart
@@ -1,6 +1,7 @@
-import 'package:api_base/presentation/widgets/widgets.dart';
+import 'package:api_base/presentation/presentation.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 @RoutePage()
 class SignInScreen extends StatelessWidget {
@@ -8,12 +9,30 @@ class SignInScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: BaseAppBar.titleAndBackButton(title: ''),
-      body: Column(
-        children: [
-          Text('Sign In'),
-        ],
+    return Scaffold(
+      appBar: const BaseAppBar.titleAndBackButton(title: 'Sign In'),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 30.h),
+          child: Column(
+            children: [
+              Container(
+                height: 200.h,
+                width: 200.w,
+                color: context.colors.backdropPrimary,
+              ),
+              16.verticalSpace,
+              Text(
+                'Let\'s You In',
+                style: AppTextStyles.headingLarge.copyWith(
+                  color: context.colors.textPrimary,
+                ),
+              ),
+              16.verticalSpace,
+              Container(),
+            ],
+          ),
+        ),
       ),
     );
   }
