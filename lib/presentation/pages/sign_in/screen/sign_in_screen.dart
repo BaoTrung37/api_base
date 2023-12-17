6c368650f2c24092d22e71c878fcdcdaa864d47f
BaoTrung <baotrung372k@gmail.com>
2023-12-17 21:52:14 +0700

    add focus

--- lib/presentation/pages/sign_in/screen/sign_in_screen.dart
+++ lib/presentation/pages/sign_in/screen/sign_in_screen.dart
@@ -1,3 +1,4 @@
+import 'package:api_base/gen/assets.gen.dart';
 import 'package:api_base/presentation/presentation.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
@@ -9,58 +10,142 @@ class SignInScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const BaseAppBar(
-        shouldShowBottomDivider: false,
-      ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.w),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            _buildLogo(context),
-            24.verticalSpace,
-            _buildHeadingTitle(),
-            24.verticalSpace,
-            const InputTextField.singleLine(
-              placeholder: 'Email',
-              prefixIcon: Icon(
-                Icons.mail,
-              ),
-              enableSuggestions: false,
-              autoCorrect: false,
+    return GestureDetector(
+      onTap: () {
+        AppUtilities.unFocus();
+      },
+      child: Scaffold(
+        resizeToAvoidBottomInset: true,
+        appBar: const BaseAppBar(
+          shouldShowBottomDivider: false,
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 30.h, horizontal: 24.w),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _buildLogo(context),
+                24.verticalSpace,
+                _buildHeadingTitle(),
+                24.verticalSpace,
+                InputTextField.singleLine(
+                  placeholder: 'Email',
+                  prefixIcon: const Icon(
+                    Icons.mail,
+                  ),
+                  enableSuggestions: false,
+                  autoCorrect: false,
+                  textInputAction: TextInputAction.next,
+                  onSubmit: (_) => FocusScope.of(context).nextFocus(),
+                ),
+                24.verticalSpace,
+                const InputTextField.singleLine(
+                  placeholder: 'Password',
+                  prefixIcon: Icon(
+                    Icons.lock_rounded,
+                  ),
+                  enableSuggestions: false,
+                  autoCorrect: false,
+                  isPassword: true,
+                ),
+                16.verticalSpace,
+                CheckboxButton(
+                  onValueChanged: (value) {
+                    print('$value');
+                  },
+                ),
+                16.verticalSpace,
+                AppButton(
+                  isExpanded: true,
+                  onTap: () {
+                    // TODO: Implement sign up method
+                  },
+                  title: 'Sign up',
+                ),
+                24.verticalSpace,
+                const AppVerticalDivider(
+                  child: Text(
+                    'or continue with',
+                  ),
+                ),
+                24.verticalSpace,
+                _buildSignUpOptions(context),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Already have an account?',
+                      style: AppTextStyles.textMedium.copyWith(
+                        color: context.colors.textPrimary,
+                      ),
+                    ),
+                    4.horizontalSpace,
+                    Text(
+                      'Sign in',
+                      style: AppTextStyles.textMedium.copyWith(
+                        color: context.colors.contentSpecialText,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-            24.verticalSpace,
-            const InputTextField.singleLine(
-              placeholder: 'Password',
-              prefixIcon: Icon(
-                Icons.lock_rounded,
-              ),
-              enableSuggestions: false,
-              autoCorrect: false,
-              isPassword: true,
-            ),
-            16.verticalSpace,
-            CheckboxButton(
-              onValueChanged: (value) {
-                print('$value');
-              },
-            ),
-            16.verticalSpace,
-            AppButton(
-              isExpanded: true,
-              onTap: () {
-                //
-              },
-              title: 'Sign up',
-            ),
-          ],
+          ),
         ),
       ),
     );
   }
 
-  Text _buildHeadingTitle() {
+  Widget _buildSignUpOptions(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildBorderButton(
+          context,
+          iconSvg: Assets.icons.icFacebook,
+          onTap: () {
+            // TODO: Implement
+          },
+        ),
+        16.horizontalSpace,
+        _buildBorderButton(
+          context,
+          iconSvg: Assets.icons.icGoogle,
+          onTap: () {
+            //
+          },
+        ),
+        16.horizontalSpace,
+        _buildBorderButton(
+          context,
+          iconSvg: Assets.icons.icApple,
+          onTap: () {
+            //
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget _buildBorderButton(
+    BuildContext context, {
+    required SvgGenImage iconSvg,
+    required VoidCallback onTap,
+  }) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 16.h),
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: context.colors.border,
+        ),
+        borderRadius: BorderRadius.circular(15.r),
+      ),
+      child: iconSvg.svg(height: 24.h),
+    );
+  }
+
+  Widget _buildHeadingTitle() {
     return const Text(
       'Create your account',
       style: AppTextStyles.headingLarge,
@@ -68,10 +153,6 @@ class SignInScreen extends StatelessWidget {
   }
 
   Widget _buildLogo(BuildContext context) {
-    return Container(
-      height: 200.h,
-      width: 200.w,
-      color: context.colors.backdropPrimary,
-    );
+    return const AppLogo();
   }
 }
