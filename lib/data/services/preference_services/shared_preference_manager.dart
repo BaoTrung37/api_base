941a7f6bf1f5c18a1d5fbd2c5bd0432c8b6c1c95
BaoTrung <baotrung372k@gmail.com>
2023-12-31 15:14:20 +0700

    add shared  preference

--- lib/data/services/preference_services/shared_preference_manager.dart
+++ lib/data/services/preference_services/shared_preference_manager.dart
@@ -23,15 +23,54 @@ class SharedPreferencesManager {
         PreferenceKeys.requestToken.name,
       );
 
-  /// Session
-  Future<void> saveSession({required String sessionId}) async =>
-      _sharedPreferences.setString(PreferenceKeys.session.name, sessionId);
+  /// Session Id
+  Future<void> saveSessionId({required String sessionId}) async =>
+      _sharedPreferences.setString(PreferenceKeys.sessionId.name, sessionId);
 
-  Future<bool> removeSession() async => _sharedPreferences.remove(
-        PreferenceKeys.session.name,
+  Future<bool> removeSessionId() async => _sharedPreferences.remove(
+        PreferenceKeys.sessionId.name,
       );
 
-  Future<String?> getSession() async => _sharedPreferences.getString(
-        PreferenceKeys.session.name,
+  Future<String?> getSessionId() async => _sharedPreferences.getString(
+        PreferenceKeys.sessionId.name,
+      );
+
+  /// Guest Session Id
+  Future<void> saveGuestSessionId({required String sessionId}) async =>
+      _sharedPreferences.setString(
+          PreferenceKeys.guestSessionId.name, sessionId);
+
+  Future<bool> removeGuestSessionId() async => _sharedPreferences.remove(
+        PreferenceKeys.guestSessionId.name,
+      );
+
+  Future<String?> getGuestSessionId() async => _sharedPreferences.getString(
+        PreferenceKeys.guestSessionId.name,
+      );
+
+  /// Access Token
+  Future<void> saveAccessToken({required String sessionId}) async =>
+      _sharedPreferences.setString(
+          PreferenceKeys.guestSessionId.name, sessionId);
+
+  Future<bool> removeAccessToken() async => _sharedPreferences.remove(
+        PreferenceKeys.guestSessionId.name,
+      );
+
+  Future<String?> getAccessToken() async => _sharedPreferences.getString(
+        PreferenceKeys.guestSessionId.name,
+      );
+
+  /// Account Id
+
+  Future<void> saveAccountId({required String sessionId}) async =>
+      _sharedPreferences.setString(PreferenceKeys.accountId.name, sessionId);
+
+  Future<bool> removeAccountId() async => _sharedPreferences.remove(
+        PreferenceKeys.accountId.name,
+      );
+
+  Future<String?> getAccountId() async => _sharedPreferences.getString(
+        PreferenceKeys.accountId.name,
       );
 }
