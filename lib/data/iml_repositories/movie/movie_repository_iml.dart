0aaec4298a962973e280365976545a250a2b4d74
BaoTrung <baotrung372k@gmail.com>
2024-01-04 23:23:55 +0700

    add movie detail api

--- lib/data/iml_repositories/movie/movie_repository_iml.dart
+++ lib/data/iml_repositories/movie/movie_repository_iml.dart
@@ -1,13 +1,26 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:api_base/data/models/movie/movie_response.dart';
 import 'package:api_base/data/services/network_services/client/rest_client.dart';
 import 'package:api_base/domain/repositories/movie/movie_repository.dart';
 import 'package:injectable/injectable.dart';
 
 @injectable
 class MovieRepositoryIml extends MovieRepository {
-  //
-  final RestClient client;
+  final RestClient restClient;
   MovieRepositoryIml({
-    required this.client,
+    required this.restClient,
   });
+
+  @override
+  Future<MovieResponse> getMovieDetail({
+    required String movieId,
+    String? appendToResponse,
+  }) async {
+    final response = await restClient.getMovieDetail(
+      movieId,
+      appendToResponse,
+    );
+
+    return response;
+  }
 }
--- lib/domain/repositories/movie/movie_repository.dart
+++ lib/domain/repositories/movie/movie_repository.dart
@@ -1,3 +1,8 @@
-abstract class MovieRepository{
-  
+import 'package:api_base/data/models/models.dart';
+
+abstract class MovieRepository {
+  Future<MovieResponse> getMovieDetail({
+    required String movieId,
+    String? appendToResponse,
+  });
 }
