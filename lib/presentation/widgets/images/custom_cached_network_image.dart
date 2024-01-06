210be1cd5c8bbcba9ea8e31d90a07e3e09a83eba
BaoTrung <baotrung372k@gmail.com>
2024-01-07 00:41:32 +0700

    add border and shape option

--- lib/presentation/widgets/images/custom_cached_network_image.dart
+++ lib/presentation/widgets/images/custom_cached_network_image.dart
@@ -1,18 +1,39 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:api_base/presentation/presentation.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class CustomCachedNetworkImage extends StatelessWidget {
   const CustomCachedNetworkImage({
     required this.imageUrl,
+    this.isBorder = false,
+    this.isCircleImage = false,
     super.key,
   });
   final String imageUrl;
+  final bool isBorder;
+  final bool isCircleImage;
+
   @override
   Widget build(BuildContext context) {
     return CachedNetworkImage(
       imageUrl: imageUrl,
       fit: BoxFit.cover,
+      imageBuilder: (context, imageProvider) {
+        return Container(
+          decoration: BoxDecoration(
+            border: isBorder
+                ? Border.all(
+                    color: context.colors.border,
+                  )
+                : null,
+            shape: isCircleImage ? BoxShape.circle : BoxShape.rectangle,
+            image: DecorationImage(
+              image: imageProvider,
+              fit: BoxFit.cover,
+            ),
+          ),
+        );
+      },
       progressIndicatorBuilder: (context, url, downloadProgress) => Center(
         child: CircularProgressIndicator(
           value: downloadProgress.progress,
