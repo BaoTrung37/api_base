d22c201574ef52dc223591775e89d39d5fab3876
BaoTrung <baotrung372k@gmail.com>
2024-01-08 23:46:17 +0700

    update string extension

--- lib/presentation/utilities/extensions/string_extension.dart
+++ lib/presentation/utilities/extensions/string_extension.dart
@@ -2,4 +2,6 @@ extension StringExtension on String {
   String get tmdbOriginPath => 'https://image.tmdb.org/t/p/original$this';
   String get tmdbW154Path => 'https://image.tmdb.org/t/p/w154$this';
   String get tmdbW300Path => 'https://image.tmdb.org/t/p/w300$this';
+  String get tmdbW500Path => 'https://image.tmdb.org/t/p/w500$this';
+  String get tmdbW1280Path => 'https://image.tmdb.org/t/p/w1280$this';
 }
