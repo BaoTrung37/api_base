d93d253acad298f195045147aea5e45440584435
BaoTrung <baotrung372k@gmail.com>
2024-01-08 17:29:47 +0700

    add observer

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,15 @@ import 'dart:async';
 
 import 'package:api_base/app.dart';
 import 'package:api_base/config/app_config.dart';
+import 'package:api_base/config/observer/bloc_observer.dart';
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runZonedGuarded(() async {
     WidgetsFlutterBinding.ensureInitialized();
     await AppConfig.getInstance().init();
+    Bloc.observer = MyBlocObserver();
     runApp(
       const MyApp(),
     );
