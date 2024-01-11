64c9a36cd79a6ec4c2346a0a2d5392975c569eed
BaoTrung <baotrung372k@gmail.com>
2024-01-11 14:45:50 +0700

    fetch similar data

--- lib/presentation/pages/movie_detail/cubit/movie_detail_cubit.dart
+++ lib/presentation/pages/movie_detail/cubit/movie_detail_cubit.dart
@@ -26,6 +26,7 @@ class MovieDetailCubit extends Cubit<MovieDetailState> {
         movieKeys: [
           MovieKeys.credits,
           MovieKeys.images,
+          MovieKeys.similar,
         ],
       ));
 
