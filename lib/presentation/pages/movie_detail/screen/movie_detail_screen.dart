5232c3d154e5501d1c8efe11c679eb3df6a17898
BaoTrung <baotrung372k@gmail.com>
2024-01-13 23:59:09 +0700

    refactor code

--- lib/presentation/pages/movie_detail/screen/movie_detail_screen.dart
+++ lib/presentation/pages/movie_detail/screen/movie_detail_screen.dart
@@ -1,15 +1,13 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:api_base/gen/assets.gen.dart';
 import 'package:api_base/injection/di.dart';
+import 'package:api_base/presentation/pages/movie_detail/widgets/widgets.dart';
 import 'package:api_base/presentation/presentation.dart';
-import 'package:api_base/presentation/utilities/extensions/string_extension.dart';
 import 'package:api_base/presentation/widgets/app_indicator/app_indicator.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:readmore/readmore.dart';
 
 @RoutePage()
 class MovieDetailScreen extends StatefulWidget {
@@ -58,7 +56,9 @@ class _MovieDetailScreenState extends State<MovieDetailScreen> {
           builder: (context, state) {
             return LoadingView(
               status: state.status,
-              child: const _MainContent(),
+              child: _MainContent(
+                movieDetailCubit: movieDetailCubit,
+              ),
             );
           },
         ),
@@ -68,21 +68,24 @@ class _MovieDetailScreenState extends State<MovieDetailScreen> {
 }
 
 class _MainContent extends StatelessWidget {
-  const _MainContent();
+  const _MainContent({
+    required this.movieDetailCubit,
+  });
 
+  final MovieDetailCubit movieDetailCubit;
   @override
   Widget build(BuildContext context) {
     return CustomScrollView(
       slivers: [
-        const _MovieInformationView(),
+        MovieInformationView(movieDetailCubit: movieDetailCubit),
         SliverToBoxAdapter(
           child: 24.verticalSpace,
         ),
-        const _FilmCastCrewView(),
+        FilmCastCrewView(movieDetailCubit: movieDetailCubit),
         SliverToBoxAdapter(
           child: 24.verticalSpace,
         ),
-        const _VideoTrailerView(),
+        VideoTrailerView(movieDetailCubit: movieDetailCubit),
         SliverToBoxAdapter(
           child: 24.verticalSpace,
         ),
@@ -90,7 +93,7 @@ class _MainContent extends StatelessWidget {
         SliverToBoxAdapter(
           child: 24.verticalSpace,
         ),
-        const _SimilarMovieView(),
+        SimilarMovieView(movieDetailCubit: movieDetailCubit),
         SliverToBoxAdapter(
           child: 24.verticalSpace,
         ),
@@ -186,300 +189,6 @@ class _MovieInformationOther extends StatelessWidget {
                     ),
                   )
                   .toList(),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class _SimilarMovieView extends StatelessWidget {
-  const _SimilarMovieView();
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<MovieDetailCubit, MovieDetailState>(
-      builder: (context, state) {
-        final similarList =
-            state.movie?.similar?.results.take(20).toList() ?? [];
-        if (similarList.isEmpty) {
-          return const SizedBox.shrink();
-        }
-        return MovieHorizontalListView(
-          padding: EdgeInsets.symmetric(horizontal: 8.w),
-          headingTitle: 'Similar',
-          movies: similarList,
-          showAllTap: () {
-            context.pushRoute(
-              ShowAllRoute(
-                argument: ShowAllArgument(title: 'Similar'),
-              ),
-            );
-          },
-          onMovieTap: (movieId) {
-            context.pushRoute(MovieDetailRoute(movieId: movieId));
-          },
-        );
-      },
-    );
-  }
-}
-
-class _FilmCastCrewView extends StatelessWidget {
-  const _FilmCastCrewView();
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<MovieDetailCubit, MovieDetailState>(
-      builder: (context, state) {
-        final casts = state.movie?.credits?.cast.take(15).toList() ?? [];
-        return SliverToBoxAdapter(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8.w),
-            child: Column(
-              children: [
-                _buildHeadingTitle(),
-                16.verticalSpace,
-                SizedBox(
-                  height: 160.h,
-                  child: ListView.separated(
-                    itemCount: casts.length,
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (context, index) {
-                      final cast = casts[index];
-                      return Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          CustomCachedNetworkImage(
-                            height: 100.r,
-                            width: 100.r,
-                            isBorder: true,
-                            imageUrl: cast.profilePath?.tmdbW300Path,
-                            imageType: ImageType.profile,
-                            isCircleImage: true,
-                          ),
-                          const Spacer(),
-                          Text(
-                            cast.originalName,
-                            style: AppTextStyles.textMediumBold,
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          Text(
-                            cast.character ?? '',
-                            style: AppTextStyles.textSmall,
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ],
-                      );
-                    },
-                    separatorBuilder: (context, index) => 16.horizontalSpace,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        );
-      },
-    );
-  }
-
-  Widget _buildHeadingTitle() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        const Text(
-          'Cast & Crew',
-          style: AppTextStyles.headingSmall,
-        ),
-        SeeAllButton(
-          onTap: () {},
-        ),
-      ],
-    );
-  }
-}
-
-class _VideoTrailerView extends StatelessWidget {
-  const _VideoTrailerView();
-
-  @override
-  Widget build(BuildContext context) {
-    return SliverToBoxAdapter(
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 8.w),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'Videos',
-              style: AppTextStyles.headingSmall,
-            ),
-            16.verticalSpace,
-            SizedBox(
-              height: 100.h,
-              child: ListView.separated(
-                itemCount: 10,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  return Column(
-                    children: [
-                      CustomCachedNetworkImage(
-                        height: 80.r,
-                        width: 150.r,
-                        imageUrl: AppConstant.posterUrl,
-                        isBorder: true,
-                      ),
-                    ],
-                  );
-                },
-                separatorBuilder: (context, index) => 16.horizontalSpace,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class _MovieInformationView extends StatelessWidget {
-  const _MovieInformationView();
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<MovieDetailCubit, MovieDetailState>(
-      buildWhen: (previous, current) => previous.movie != current.movie,
-      builder: (context, state) {
-        return SliverToBoxAdapter(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 220.h,
-                width: double.infinity,
-                child: CustomCachedNetworkImage(
-                  imageUrl: state.movie?.backdropPath?.tmdbW1280Path,
-                ),
-              ),
-              Transform.translate(
-                offset: Offset(0, -10.h),
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 8.w),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        height: 150.h,
-                        width: 90.w,
-                        child: CustomCachedNetworkImage(
-                          imageUrl: state.movie?.posterPath?.tmdbW500Path,
-                        ),
-                      ),
-                      8.horizontalSpace,
-                      Expanded(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              state.movie?.title ?? '',
-                              style: AppTextStyles.headingSmall,
-                            ),
-                            4.verticalSpace,
-                            _buildMovieRate(state),
-                            8.verticalSpace,
-                            _buildMovieGenres(context, state),
-                            8.verticalSpace,
-                            ReadMoreText(
-                              state.movie?.overview ?? '',
-                              trimLines: 5,
-                              trimMode: TrimMode.Line,
-                              trimCollapsedText: '.',
-                              trimExpandedText: '.',
-                              callback: (val) {
-                                print(val);
-                              },
-                              moreStyle: AppTextStyles.textMedium,
-                            ),
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
-
-  Widget _buildMovieGenres(BuildContext context, MovieDetailState state) {
-    final genres = state.movie?.genres ?? [];
-    if (genres.isEmpty) {
-      return const SizedBox.shrink();
-    }
-    return Wrap(
-      runSpacing: 8.h,
-      spacing: 8.w,
-      children: genres
-          .map(
-            (e) => Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: context.colors.border,
-                  width: 1.5,
-                ),
-              ),
-              padding: EdgeInsets.symmetric(horizontal: 8.w, vertical: 6.h),
-              child: Text(
-                e.name,
-                style: AppTextStyles.labelMediumLight,
-              ),
-            ),
-          )
-          .toList(),
-    );
-  }
-
-  Widget _buildMovieRate(MovieDetailState state) {
-    return Row(
-      children: [
-        RatingBar(
-          initialRating: (state.movie?.voteAverage ?? 0) / 10 * 5,
-          direction: Axis.horizontal,
-          allowHalfRating: true,
-          itemCount: 5,
-          ratingWidget: RatingWidget(
-            full: Assets.icons.icStar.svg(),
-            half: Assets.icons.icStarHalf.svg(),
-            empty: Assets.icons.icStarOutline.svg(),
-          ),
-          ignoreGestures: true,
-          itemSize: 12.sp,
-          itemPadding: EdgeInsets.symmetric(horizontal: 2.w),
-          onRatingUpdate: (_) {},
-        ),
-        8.horizontalSpace,
-        Text(
-          '( ${state.movie?.voteCount.toString() ?? '0'} )',
-          style: AppTextStyles.textSmallBold,
-        ),
-        Expanded(
-          child: Center(
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Assets.icons.icStar.svg(height: 12.sp),
-                8.horizontalSpace,
-                Text(
-                  state.movie?.voteAverage.toString() ?? '0',
-                  style: AppTextStyles.textMediumBold,
-                ),
-              ],
             ),
           ),
         ),
---
+++ lib/presentation/pages/movie_detail/widgets/widgets.dart
@@ -0,0 +1,4 @@
+export 'film_cast_crew_view.dart';
+export 'movie_information_view.dart';
+export 'similar_movie_view.dart';
+export 'video_trailer_view.dart';
