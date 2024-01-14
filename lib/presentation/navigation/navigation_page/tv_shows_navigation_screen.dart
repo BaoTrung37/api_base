31ca410c22fe52d534c06bbe30f333a913dc2c92
BaoTrung <baotrung372k@gmail.com>
2024-01-14 18:55:48 +0700

    add navigation page

---
+++ lib/presentation/navigation/navigation_page/tv_shows_navigation_screen.dart
@@ -0,0 +1,12 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+class TvShowsNavigationScreen extends StatelessWidget {
+  const TvShowsNavigationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const AutoRouter();
+  }
+}
