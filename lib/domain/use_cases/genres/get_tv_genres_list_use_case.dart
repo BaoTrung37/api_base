ff425ab5b23e0c8f2dc6820eb57f4b96f0b7b4eb
BaoTrung <baotrung372k@gmail.com>
2024-01-15 12:54:48 +0700

    fetch data

--- lib/domain/use_cases/genres/get_tv_genres_list_use_case.dart
+++ lib/domain/use_cases/genres/get_tv_genres_list_use_case.dart
@@ -1,11 +1,10 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:api_base/data/iml_repositories/genres/genres_repository_imp.dart';
-import 'package:api_base/data/models/models.dart';
 import 'package:api_base/domain/use_cases/base/use_case.dart';
 import 'package:injectable/injectable.dart';
 
 @injectable
-class GetTvGenresListUseCase extends FutureOutputUseCase<GenresResponse> {
+class GetTvGenresListUseCase extends FutureOutputUseCase<void> {
   final GenresRepositoryImp genresRepositoryImp;
 
   GetTvGenresListUseCase({
@@ -13,7 +12,7 @@ class GetTvGenresListUseCase extends FutureOutputUseCase<GenresResponse> {
   });
 
   @override
-  Future<GenresResponse> run() {
+  Future<void> run() {
     return genresRepositoryImp.getTvGenresList();
   }
 }
