8b4b9e95f3e910c9ffddc90064b5b8fdc8a2bbc1
BaoTrung <baotrung372k@gmail.com>
2024-01-16 13:47:50 +0700

    add list to state

--- lib/presentation/pages/show_all/cubits/show_all/show_all_state.dart
+++ lib/presentation/pages/show_all/cubits/show_all/show_all_state.dart
@@ -5,6 +5,8 @@ class ShowAllState with _$ShowAllState {
   const factory ShowAllState({
     @Default(AppStatus.initial) AppStatus status,
     @Default([]) List<MovieResponse> similarMovieList,
+    @Default([]) List<MovieResponse> popularMovieList,
+    @Default([]) List<MovieResponse> nowPlayingMovieList,
     AppError? appError,
   }) = _ShowAllState;
 }
