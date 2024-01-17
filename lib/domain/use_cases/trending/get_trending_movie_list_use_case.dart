f15cbde935c6b05787f08dbd37126f049a7acf39
BaoTrung <baotrung372k@gmail.com>
2024-01-18 00:12:11 +0700

    add use case

---
+++ lib/domain/use_cases/trending/get_trending_movie_list_use_case.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:api_base/data/iml_repositories/trending/trending_repository_iml.dart';
+import 'package:api_base/data/models/models.dart';
+import 'package:api_base/domain/use_cases/use_cases.dart';
+import 'package:api_base/presentation/utilities/enums/time_type.dart';
+
+class GetTrendingMovieListUseCase
+    implements FutureUseCase<GetTrendingMovieListInput, List<MovieResponse>> {
+  GetTrendingMovieListUseCase({required this.trendingRepositoryIml});
+
+  final TrendingRepositoryIml trendingRepositoryIml;
+  @override
+  Future<List<MovieResponse>> run(GetTrendingMovieListInput input) {
+    return trendingRepositoryIml.getTrendingMovieList(
+      page: input.page,
+      timeWindow: input.timeWindow,
+    );
+  }
+}
+
+class GetTrendingMovieListInput {
+  final TimeType timeWindow;
+  final int page;
+  GetTrendingMovieListInput({
+    required this.page,
+    this.timeWindow = TimeType.day,
+  });
+}
