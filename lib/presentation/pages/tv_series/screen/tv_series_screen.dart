03f754077aae8a764fc16fa0564249c30b5a9324
BaoTrung <baotrung372k@gmail.com>
2024-01-28 21:51:14 +0700

    update tv response

--- lib/presentation/pages/tv_series/screen/tv_series_screen.dart
+++ lib/presentation/pages/tv_series/screen/tv_series_screen.dart
@@ -1,3 +1,5 @@
+import 'package:api_base/injection/di.dart';
+import 'package:api_base/presentation/pages/tv_series/cubit/tv_series_controller_cubit.dart';
 import 'package:api_base/presentation/presentation.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
@@ -11,43 +13,116 @@ class TvSeriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
-      providers: const [
-        // BlocProvider(
-        //   create: (context) => getIt.get<PopularMovieCubit>(),
-        // ),
-        // BlocProvider(
-        //   create: (context) => SubjectBloc(),
-        // ),
+      providers: [
+        BlocProvider(
+          create: (context) =>
+              getIt.get<TvSeriesControllerCubit>()..fetchData(),
+        ),
       ],
       child: Scaffold(
         appBar: const BaseAppBar.titleOnly(title: 'Tv Shows'),
-        body: CustomScrollView(
-          slivers: [
-            // BlocBuilder<PopularMovieCubit, MovieState>(
-            //   buildWhen: (previous, current) =>
-            //       previous.status != current.status,
-            //   builder: (context, state) {
-            //     return MovieHorizontalListView(
-            //       headingTitle: 'Popular',
-            //       movies: state.movies,
-            //       onMovieTap: (movieId) {
-            //         context.pushRoute(MovieDetailRoute(movieId: movieId));
-            //       },
-            //       showAllTap: () {
-            //         context.pushRoute(
-            //           ShowAllRoute(
-            //             argument: ShowAllArgument.movie(
-            //               title: 'Popular',
-            //               apiMovieType: ApiMovieType.popular,
-            //             ),
-            //           ),
-            //         );
-            //       },
-            //     );
-            //   },
-            // ),
-            SliverToBoxAdapter(child: 24.verticalSpace),
-          ],
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.w),
+          child: CustomScrollView(
+            slivers: [
+              BlocBuilder<TvSeriesControllerCubit, TvSeriesControllerState>(
+                buildWhen: (previous, current) =>
+                    previous.status != current.status,
+                builder: (context, state) {
+                  return TvSeriesHorizontalListView(
+                    headingTitle: 'Airing Today',
+                    tvSeriesList: state.airingTodayList,
+                    onTvSeriesTap: (movieId) {
+                      // context.pushRoute(MovieDetailRoute(movieId: movieId));
+                    },
+                    showAllTap: () {
+                      // context.pushRoute(
+                      //   ShowAllRoute(
+                      //     argument: ShowAllArgument.movie(
+                      //       title: 'Airing Today',
+                      //       apiMovieType: ApiMovieType.popular,
+                      //     ),
+                      //   ),
+                      // );
+                    },
+                  );
+                },
+              ),
+              SliverToBoxAdapter(child: 24.verticalSpace),
+              BlocBuilder<TvSeriesControllerCubit, TvSeriesControllerState>(
+                buildWhen: (previous, current) =>
+                    previous.status != current.status,
+                builder: (context, state) {
+                  return TvSeriesHorizontalListView(
+                    headingTitle: 'On The Air',
+                    tvSeriesList: state.onTheAirList,
+                    onTvSeriesTap: (movieId) {
+                      // context.pushRoute(MovieDetailRoute(movieId: movieId));
+                    },
+                    showAllTap: () {
+                      // context.pushRoute(
+                      //   ShowAllRoute(
+                      //     argument: ShowAllArgument.movie(
+                      //       title: 'Airing Today',
+                      //       apiMovieType: ApiMovieType.popular,
+                      //     ),
+                      //   ),
+                      // );
+                    },
+                  );
+                },
+              ),
+              SliverToBoxAdapter(child: 24.verticalSpace),
+              BlocBuilder<TvSeriesControllerCubit, TvSeriesControllerState>(
+                buildWhen: (previous, current) =>
+                    previous.status != current.status,
+                builder: (context, state) {
+                  return TvSeriesHorizontalListView(
+                    headingTitle: 'Popular',
+                    tvSeriesList: state.popularList,
+                    onTvSeriesTap: (movieId) {
+                      // context.pushRoute(MovieDetailRoute(movieId: movieId));
+                    },
+                    showAllTap: () {
+                      // context.pushRoute(
+                      //   ShowAllRoute(
+                      //     argument: ShowAllArgument.movie(
+                      //       title: 'Airing Today',
+                      //       apiMovieType: ApiMovieType.popular,
+                      //     ),
+                      //   ),
+                      // );
+                    },
+                  );
+                },
+              ),
+              SliverToBoxAdapter(child: 24.verticalSpace),
+              BlocBuilder<TvSeriesControllerCubit, TvSeriesControllerState>(
+                buildWhen: (previous, current) =>
+                    previous.status != current.status,
+                builder: (context, state) {
+                  return TvSeriesHorizontalListView(
+                    headingTitle: 'Top Rated',
+                    tvSeriesList: state.topRatedList,
+                    onTvSeriesTap: (movieId) {
+                      // context.pushRoute(MovieDetailRoute(movieId: movieId));
+                    },
+                    showAllTap: () {
+                      // context.pushRoute(
+                      //   ShowAllRoute(
+                      //     argument: ShowAllArgument.movie(
+                      //       title: 'Airing Today',
+                      //       apiMovieType: ApiMovieType.popular,
+                      //     ),
+                      //   ),
+                      // );
+                    },
+                  );
+                },
+              ),
+              SliverToBoxAdapter(child: 24.verticalSpace),
+            ],
+          ),
         ),
       ),
     );
