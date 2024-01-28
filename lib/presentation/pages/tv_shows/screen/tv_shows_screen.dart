02cefd5b8b3c0e41231aa2b2882d2cffb319cd4f
BaoTrung <baotrung372k@gmail.com>
2024-01-28 12:53:45 +0700

    init tv show view

--- lib/presentation/pages/tv_shows/screen/tv_shows_screen.dart
+++ lib/presentation/pages/tv_shows/screen/tv_shows_screen.dart
@@ -1,6 +1,9 @@
+import 'package:api_base/injection/di.dart';
 import 'package:api_base/presentation/presentation.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 @RoutePage()
 class TvShowsScreen extends StatelessWidget {
@@ -8,8 +11,46 @@ class TvShowsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: BaseAppBar.titleOnly(title: 'Tv Shows'),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => getIt.get<PopularMovieCubit>(),
+        ),
+        // BlocProvider(
+        //   create: (context) => SubjectBloc(),
+        // ),
+      ],
+      child: Scaffold(
+        appBar: const BaseAppBar.titleOnly(title: 'Tv Shows'),
+        body: CustomScrollView(
+          slivers: [
+            BlocBuilder<PopularMovieCubit, MovieState>(
+              buildWhen: (previous, current) =>
+                  previous.status != current.status,
+              builder: (context, state) {
+                return MovieHorizontalListView(
+                  headingTitle: 'Popular',
+                  movies: state.movies,
+                  onMovieTap: (movieId) {
+                    context.pushRoute(MovieDetailRoute(movieId: movieId));
+                  },
+                  showAllTap: () {
+                    context.pushRoute(
+                      ShowAllRoute(
+                        argument: ShowAllArgument.movie(
+                          title: 'Popular',
+                          apiMovieType: ApiMovieType.popular,
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+            SliverToBoxAdapter(child: 24.verticalSpace),
+          ],
+        ),
+      ),
     );
   }
 }
