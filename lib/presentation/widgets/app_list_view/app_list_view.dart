1cb6e5235775f535fcc1f6c6be1965a1404de702
BaoTrung <baotrung372k@gmail.com>
2024-01-28 15:26:11 +0700

    add tv shows horizontal list view

--- lib/presentation/widgets/app_list_view/app_list_view.dart
+++ lib/presentation/widgets/app_list_view/app_list_view.dart
@@ -1,3 +1,4 @@
 export 'circle_horizontal_list_view.dart';
 export 'loading_list_view.dart';
 export 'movie_horizontal_list_view.dart';
+export 'tv_shows_horizontal_list_view.dart';
---
+++ lib/presentation/widgets/app_list_view/tv_shows_horizontal_list_view.dart
@@ -0,0 +1,160 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:api_base/data/models/tv_series/tv_series_response.dart';
+import 'package:api_base/presentation/presentation.dart';
+import 'package:api_base/presentation/utilities/extensions/string_extension.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+typedef TvSeriesFunction = Function(int seriesId);
+
+class TvSeriesHorizontalListView extends StatelessWidget {
+  const TvSeriesHorizontalListView({
+    required this.headingTitle,
+    required this.tvSeriesList,
+    required this.onTvSeriesTap,
+    super.key,
+    this.height,
+    this.width,
+    this.padding,
+    this.showAllTap,
+    this.isPoster = true,
+  });
+
+  final String headingTitle;
+  final List<TvSeriesResponse> tvSeriesList;
+
+  final double? height;
+  final double? width;
+
+  final EdgeInsetsGeometry? padding;
+
+  final VoidCallback? showAllTap;
+  final TvSeriesFunction onTvSeriesTap;
+  final bool isPoster;
+
+  double _getWidthItem(BuildContext context) {
+    final size = MediaQuery.sizeOf(context).width;
+    if (isPoster) {
+      return size * 0.3;
+    }
+    return size * 0.45;
+  }
+
+  double _getHeightItem() {
+    return isPoster ? 230.h : 180.h;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Container(
+        padding: padding,
+        child: Column(
+          children: [
+            _buildHeadingTitle(),
+            16.verticalSpace,
+            if (tvSeriesList.isNotEmpty)
+              _buildMovieListView()
+            else
+              LoadingListView(
+                height: height ?? _getHeightItem(),
+                width: width ?? _getWidthItem(context),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMovieListView() {
+    return SizedBox(
+      height: height ?? _getHeightItem(),
+      width: double.infinity,
+      child: CarouselSlider.builder(
+        options: CarouselOptions(
+          scrollDirection: Axis.horizontal,
+          enableInfiniteScroll: true,
+          enlargeCenterPage: true,
+          viewportFraction: isPoster ? 0.35 : 0.5,
+          // autoPlay: true,
+          // autoPlayInterval: const Duration(seconds: 3),
+          // autoPlayAnimationDuration: const Duration(milliseconds: 800),
+          // autoPlayCurve: Curves.fastOutSlowIn,
+        ),
+        itemBuilder: (context, index, pageViewIndex) {
+          final tvSeries = tvSeriesList[index];
+          return _buildTvSeriesItem(context, tvSeries, onTvSeriesTap);
+        },
+        itemCount: tvSeriesList.length,
+      ),
+    );
+  }
+
+  Widget _buildTvSeriesItem(
+    BuildContext context,
+    TvSeriesResponse movie,
+    TvSeriesFunction onMovieTap,
+  ) {
+    return GestureDetector(
+      onTap: () {
+        onMovieTap.call(movie.id);
+      },
+      child: SizedBox(
+        height: height ?? _getHeightItem(),
+        width: width ?? _getWidthItem(context),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Expanded(
+              child: CustomCachedNetworkImage(
+                imageType: ImageType.movie,
+                imageUrl: isPoster
+                    ? movie.posterPath.tmdbW154Path
+                    : movie.backdropPath.tmdbW300Path,
+              ),
+            ),
+            8.verticalSpace,
+            SizedBox(
+              height: 70.h,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    movie.name,
+                    style: AppTextStyles.labelMedium,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  4.verticalSpace,
+                  Text(
+                    movie.genresName,
+                    style: AppTextStyles.labelSmallLight,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildHeadingTitle() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          headingTitle,
+          style: AppTextStyles.headingSmall,
+        ),
+        SeeAllButton(
+          onTap: () => showAllTap?.call(),
+        ),
+      ],
+    );
+  }
+}
